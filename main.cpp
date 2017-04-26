3a57696c573974a721a5777d74ae6ebfe9b59392
OscarZhou <OscarZhou@users.noreply.github.com>
2017-04-26 19:17:34 +1200

    Add files via upload
    finish the code , but there is a problem on the s1,s2, I dont know why, need to debug it again.

--- main.cpp
+++ main.cpp
@@ -16,51 +16,68 @@ using namespace cv;
 #define MpixelG(image, x, y) ((uchar *)(((image).data)+(y)*((image).step)))[(x)*((image).channels())+1]
 #define MpixelR(image, x, y) ((uchar *)(((image).data)+(y)*((image).step)))[(x)*((image).channels())+2]
 
-
-typedef std::set<Point> point_set;
-typedef vector<point_set> set_vector;
-
-
-class Point
+class CPoint
 {
     public:
 
         int x;
         int y;
 
-        Point();
-        virtual ~Point();
+        CPoint();
+        CPoint(int x, int y);
+        virtual ~CPoint();
 
-        Point(int x, int y) {this->x = x; this->y = y;};
-        bool operator< (const Point& pt) const{
-           if(this->x < pt.x)
-            {
-                return true;
-            }
-            else if(this->x == pt.x)
-            {
-                if(this->y < pt.y)
-                {
-                    return true;
-                }
-                else if(this->y == pt.y)
-                {
-                    return false;
-                }
-                else{
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-        };
+        bool operator< (const CPoint& pt) const;
+
 
     protected:
     private:
 };
 
+CPoint::CPoint()
+{
+
+}
+
+CPoint::~CPoint()
+{
+
+}
+
+CPoint::CPoint(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+bool CPoint::operator< (const CPoint& pt) const
+{
+   if(this->x < pt.x)
+    {
+        return true;
+    }
+    else if(this->x == pt.x)
+    {
+        if(this->y < pt.y)
+        {
+            return true;
+        }
+        else if(this->y == pt.y)
+        {
+            return false;
+        }
+        else{
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+};
+
+typedef std::set<CPoint> point_set;
+typedef vector<point_set> set_vector;
 
 /************************************************************************
 *
@@ -69,6 +86,7 @@ class Point
 *************************************************************************/
 Mat do_median_filter(Mat imgOri);
 Mat do_threshold_filter(Mat imgOri, int flag);
+int count_object(Mat imgOri);
 
 int main(int argc, char** argv)
 {
@@ -77,18 +95,24 @@ int main(int argc, char** argv)
         exit(0);
 	}
 
-    namedWindow("OriginalImage", 0);
+    namedWindow("Step1: OriginalImage", 0);
     Mat imgOri = imread(argv[1], IMREAD_GRAYSCALE);
-	imshow("OriginalImage", imgOri);
+	imshow("Step1: OriginalImage", imgOri);
 
-    namedWindow("MedianFilterImage", 0);
+    namedWindow("Step2: MedianFilterImage", 0);
     Mat imgMedianFilter = do_median_filter(imgOri);
-	imshow("MedianFilterImage", imgMedianFilter);
+	imshow("Step2: MedianFilterImage", imgMedianFilter);
 
-    namedWindow("ThresholdFilterImage", 0);
+    namedWindow("Step3: ThresholdFilterImage", 0);
     Mat imgThresholdFilter = do_threshold_filter(imgMedianFilter, 0);
-	imshow("ThresholdFilterImage", imgThresholdFilter);
+	imshow("Step3: ThresholdFilterImage", imgThresholdFilter);
 
+    int num = count_object(imgThresholdFilter);
+    cout<<"the number is "<<num<<endl;
+
+    char printit[100];
+    sprintf(printit,"%d",num);
+    putText(imgThresholdFilter, printit, cvPoint(10,30), FONT_HERSHEY_PLAIN, 2, cvScalar(255,255,255), 2, 8);
 
     waitKey();
     return 0;
@@ -212,9 +236,10 @@ Mat do_median_filter(Mat imgOri)
 
 /************************************************************************
 *
-* The impletation of median filter
+* The impletation of threshold filter
 * Parameter description:
-* imgOri : input image with salt and pepper
+* imgOri : input image
+* flag: 0, Otsu method  1, normal method
 *
 *************************************************************************/
 Mat do_threshold_filter(Mat imgOri, int flag)
@@ -230,15 +255,127 @@ Mat do_threshold_filter(Mat imgOri, int flag)
 
 /************************************************************************
 *
-* The impletation of median filter
+* The impletation of blob algorithm
 * Parameter description:
 * imgOri : input image with salt and pepper
 *
 *************************************************************************/
+int count_object(Mat imgOri)
+{
+    // Store all the sets which stand for the object
+    set_vector vec;
+    int counter = -1, s1, s2;
+
+    int width = imgOri.cols + 1;
+    int height = imgOri.rows + 1;
+    int* matrixA = new int[width * height];
+
+    Mat imgBoundry;
+    imgBoundry.create(height, width, CV_8UC1);
+
+    /************************************************************************
+    *
+    * Move image into a new one with top and left boundary
+    *
+    *************************************************************************/
+    for(int y=0; y< height; y++)
+        for(int x=0; x<width; x++)
+        {
+            if(x==0 || y==0 )
+            {
+                Mpixel(imgBoundry, x, y) = 0;
+            }
+            else
+            {
+                Mpixel(imgBoundry, x, y) = Mpixel(imgOri, x-1, y-1);
+            }
+            matrixA[x + y * width] = -1;
+            //if(x==100 && y== 101 )
+            //    cout<<"x + y * width= "<<x<<" + "<<y<<" * "<<width<<" = "<<matrixA[x + y * width]<<endl;
+
+        }
+
+    namedWindow("Step4: ImageWithBoundary", 0);
+	imshow("Step4: ImageWithBoundary", imgBoundry);
+
+    /************************************************************************
+    *
+    * The implementation of object labeling algorithm using 4-adjacency
+    *
+    *************************************************************************/
+    for(int y=1; y<height; y++)
+    {
+        for(int x=1; x<width; x++)
+        {
+            if((int)Mpixel(imgBoundry, x, y)  != 0)
+            {
+                if(Mpixel(imgBoundry, x-1, y) != 0 || Mpixel(imgBoundry, x, y-1) != 0)
+                {
+                    //cout<<"------"<<matrixA[216266]<<endl;
+                    s1 = matrixA[(x - 1) + (y * width)];
+                    s2 = matrixA[x +  ((y - 1) * width)];
+
+                    cout<<"s1 = x - 1 + y * width ="<<x<<" + "<<y<<" * "<<width<<" = "<<(x - 1) + (y * width) << " , s1="<< *(matrixA +(x-1)+(y*width))<<endl;
+                    //cout<<"s2 = x + (y - 1) * width"<<x<<" + "<<y<<" * "<<width<<" = "<<matrixA[x +  ((y - 1) * width)]<< " , s2="<<s2<<endl;
+
+                    if(s1 != -1)
+                    {
+                        point_set* pset = &vec[s1];
+                        pset->insert(CPoint(x, y));
+                        matrixA[x + y * width] = s1;
+                    }
+
+                    if(s2 != -1)
+                    {
+                        point_set* pset = &vec[s2];
+                        pset->insert(CPoint(x, y));
+                        matrixA[x + y * width] = s2;
+                    }
+
+                    if((s1 != s2) && (s1 != -1) && (s2 != -1))
+                    {
+                        point_set* pset = &vec[s2];
+                        for(point_set::iterator it=pset->begin(); it!=pset->end(); it++)
+                        {
+                           ((point_set)vec[s1]).insert(*it);
+                            matrixA[((CPoint)*it).x + ((CPoint)*it).y * width] = s1;
+                        }
+
+                        pset->clear();
+                        pset = NULL;
+                    }
+                }
+            }
+            else
+            {
+                counter++;
+                point_set setofobj;
+                setofobj.insert(CPoint(x, y));
+                vec.push_back(setofobj);
+                matrixA[x + y * imgBoundry.cols] = counter;
+            }
+        }
+    }
+    free(matrixA);
+    /************************************************************************
+    *
+    * count the number of valid set, that is, the number of objects
+    *
+    *************************************************************************/
+    int num = 0;
+    if(!vec.empty())
+    {
+        cout<<"vec.empty "<<vec.empty()<<endl;
+        for(set_vector::iterator it=vec.begin(); it!=vec.end(); it++)
+        {
+            point_set poset = *it;
+            //cout<<"the numbers of points in poset: "<<poset.size()<<endl;
+            if(!poset.empty() && poset.size() > 30)
+                num++;
+        }
+    }
 
 
-
-
-
-
+	return num;
+}
 
